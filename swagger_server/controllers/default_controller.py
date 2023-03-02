fe377b6da53d899637a390f3fa426bc64756bfba
Anthony Kirkinis <akirkinis@gmail.com>
2023-03-02 10:56:41 +0100

    updated

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -32,7 +32,7 @@ def delete_student(student_id):  # noqa: E501
 
     :rtype: None
     """
-    return "okie dokie"
+    return delete(student_id)
 
 
 def get_student_by_id(student_id):  # noqa: E501
@@ -45,4 +45,4 @@ def get_student_by_id(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return student_id
+    return get_by_id(student_id)
